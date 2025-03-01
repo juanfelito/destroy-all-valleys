16313f840183c30050402c583c8a277638905953
Felipe Balcero <felipe.balcero45@gmail.com>
2025-03-01 11:15:24 -0500

    Item list

--- Assets/Scripts/Types/Enums.cs
+++ Assets/Scripts/Types/Enums.cs
@@ -1,14 +1,27 @@
-public enum ToolEffect
-{
+public enum ToolEffect {
     none,
     watering
 }
 
-public enum Direction
-{
+public enum Direction {
     up,
     down,
     left,
     right,
     none
+}
+
+public enum ItemType {
+    Seed,
+    Commodity,
+    Watering_tool,
+    Hoeing_tool,
+    Chopping_tool,
+    Breaking_tool,
+    Reaping_tool,
+    Collecting_tool,
+    Reapable_scenery,
+    Furniture,
+    none,
+    count
 }
